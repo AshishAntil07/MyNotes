7b4f113ea6ff22e58299627ca71d531854927664
Ashish Antil <ashishantil.antil07@gmail.com>
2022-10-23 12:45:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>My Notes</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="MyNotes/Notes.css">
+    <link rel="shortcut icon" type="image/x-icon">
+  </head>
+  <body>
+    <div class="message">Copied!</div>
+    <div class="Header">
+      <img class="HeaderIMG" alt="logo"><span>My Notes</span>
+      <div class="formatter"></div>
+    </div>
+    <div id="addNote">
+      <span class="Plus">
+        <span>+</span>
+      </span>
+      <span class = "ANText">Add Note</span>
+    </div>
+    <div id="notesContainer"></div>
+
+    <script src="MyNotes/Notes.js" async="false"></script>
+  </body>
+</html>
